603f60afdc52d207c0b43052b9b63fb15c334f70
MattySanchez20 <matias.sanchez@protonmail.com>
2023-04-24 15:58:41 +0100

    adding new files

---
+++ api_tik.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.tiktok.com/foryou")
+
+print(driver.title)
+
+# driver.quit()
